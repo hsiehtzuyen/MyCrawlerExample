2e772483845260ac415b1623555d035e6560d22b
hsiehtzuyen <phoebe25852@hotmail.com>
2017-12-10 10:09:20 +0800

    2017.12.10練習

--- src/crawler/example/integration/FBElasticImport.java
+++ src/crawler/example/integration/FBElasticImport.java
@@ -12,16 +12,16 @@ public class FBElasticImport {
 
     static String elasticHost = "dyn.gibar.co" ;
     static String elasticPort = "9200" ;
-    static String elasticIndex = "abola2";
+    static String elasticIndex = "hsiehtzuyen";
     static String elasticIndexType = "data";
-    static String pageName = "JudgeAd";
+    static String pageName = "edn168";
     // 2017-09-02
-    static long start = 1504363907;
+    static long start = 1512871743;
     // 往前抓抓取日期數
     static int days = 10;
     // 每日抓取文章上限 (上限1000)
     static int maxPosts = 10;
-    static String access_token = "EAACEdEose0cBAAuJhUlaZCuuqYnzXXXKPGY7Es4KZCLBLpnrIdCWjutfSl1AFZAdR3pFnF6PdrfmaU2EwyllSxhKAd7ZCIGfxZCeg8VkdspelNr9vVWIZCeBRnU5KUlZAPh9zamyT836FmdCadDfzwFILwR6hdMse7irLpIto5cnUnqSV4MtEAA2Hf8slFdXZBS489aSanZCYLAZDZD";
+    static String access_token = "EAACEdEose0cBAPEJhTMGr7bgs1gm3nFsfY7zJwGdgY2yyAGZB2SobHuPLC8TrZBI3RELGHpnp9zLTKlbp76AscDIUAajgAzE6Xb9seMzvx3AfPi0PMrLkpFFRAoEIIA2AKpIqKcGRliuUoFVRX6FhJQMRZC3blGEJ07aKWNst1HTYuwwZByXaYZBsH6luBMAZD";
 
     public static void main(String[] args) {
 
